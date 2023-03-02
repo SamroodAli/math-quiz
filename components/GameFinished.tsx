5e58fe1e76bebd068e09d28984ae4f71a807c21c
samroodAli <devsamrood@gmail.com>
2023-03-02 21:44:20 +0530

    fix: remove new game option added without appropriate states

--- components/GameFinished.tsx
+++ components/GameFinished.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export const GameFinished = () => {
   return (
     <div>
       <p>Oops, You lost</p>
-      <Link href="/">Start a new game</Link>
     </div>
   );
 };
--- components/GameWon.tsx
+++ components/GameWon.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export const GameWon = () => {
   return (
     <div>
       <p>Yay, you won</p>
-      <Link href="/">Create a new game</Link>
     </div>
   );
 };
